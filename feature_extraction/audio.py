ca0ba284e015d32119d7195a6ee240c78ee686f3
aagnone <anthonyagnone@gmail.com>
2019-03-06 16:33:36 -0500

    Extract on multiple windows per file

--- feature_extraction/audio.py
+++ feature_extraction/audio.py
@@ -10,13 +10,19 @@ from librosa.util import normalize
 from librosa import filters
 
 
+def zero_fill(arr, target_shape):
+    expansions = np.array([
+        [0, target_shape[i] - arr.shape[i]]
+        for i in range(len(target_shape))
+    ])
+    return np.pad(arr, expansions, mode='constant', constant_values=0)
+
 
 def log_mel_fbe(fn, return_values=False, output_dir=None, sampling_rate=44100,
-               nfft=1024, n_mels=64, duration=2, frame_length=1101, hop_length=661, window=None):
+               nfft=1024, n_mels=64, duration=2, frame_length=1101, hop_length=661, window=None, force=False):
 
     f_to_mel = filters.mel(sr=sampling_rate, n_fft=nfft, n_mels=n_mels)
     overlap_length = frame_length - hop_length
-    input_length = int(sampling_rate * duration)
 
     def extract(audio_fn):
         # Read and Resample the audio
@@ -26,18 +32,14 @@ def log_mel_fbe(fn, return_values=False, output_dir=None, sampling_rate=44100,
         except:
             return None
 
-        # Random offset / Padding
-        # TODO get all windows
-        if len(data) > input_length:
-            max_offset = len(data) - input_length
-            offset = np.random.randint(max_offset)
-            data = data[offset:(input_length+offset)]
-        else:
-            if input_length > len(data):
-                max_offset = input_length - len(data)
-                offset = np.random.randint(max_offset)
-                data = np.pad(data, (offset, input_length - len(data) - offset), "constant")
+        # zero-padding to a multiple of <duration>-second chunks
+        spectral_chunk_size = int(sampling_rate * duration)
+        n_spectral_chunks = (1 + len(data) // spectral_chunk_size)
+        target_length = sampling_rate * (duration * n_spectral_chunks)
+        edge_pad_length = int(frame_length/2)
+        data = np.pad(data, ((edge_pad_length, edge_pad_length)), mode='constant', constant_values=0)
 
+        # spectrogram
         f, t, Sxx = sp.signal.spectrogram(
             data,
             fs=sampling_rate,
@@ -46,31 +48,46 @@ def log_mel_fbe(fn, return_values=False, output_dir=None, sampling_rate=44100,
             noverlap=overlap_length,
             nfft=nfft
         )
+
+        # spectrogram -> log mel fb
         mel_spec = f_to_mel.dot(Sxx)
         log_mel_spec = np.log(1e-8 + mel_spec)
         log_mel_spec = np.expand_dims(log_mel_spec, axis=-1)
-        return log_mel_spec
+        spectral_chunk_hop_length = int(int(sampling_rate * 0.500) / hop_length)
+        n_frames_per_chunk = int(spectral_chunk_size / hop_length)
+        spectral_chunks = [
+            zero_fill(log_mel_spec[:, i:i + n_frames_per_chunk], (n_mels, n_frames_per_chunk, 1))
+            for i in range(0, log_mel_spec.shape[1], spectral_chunk_hop_length)
+        ]
 
-    out_fn = None
-    if output_dir:
+        return spectral_chunks
+
+    out_fns = list()
+    if output_dir is not None:
         # create the output directory if it does not exist
         if not path.exists(output_dir):
             os.mkdir(output_dir)
 
-        # store the features at the output file, if not already present
-        out_fn = path.join(output_dir, path.split(fn)[-1] + ".npz")
-        log_mel_spec = extract(fn)
-        if log_mel_spec is None:
-            return "BAD_" + out_fn
+        # extract and store the features at the output file(s), only if not already present
+        fn_template = path.join(output_dir, path.split(fn)[-1] + ".npz")
+        spectra = extract(fn)
 
-        if not path.exists(out_fn):
-            np.savez_compressed(out_fn, spec=extract(fn))
+        if spectra is None:
+            return "BAD_" + fn_template
+
+        def indexed_sub_fn(fn, i):
+            prefix, ext = path.splitext(fn)
+            return prefix + str(i) + ext
+
+        for i, spec in enumerate(spectra):
+            out_fns.append(indexed_sub_fn(fn_template, i))
+            if not path.exists(out_fns[-1]) or force:
+                np.savez_compressed(out_fns[-1], spec=spec)
     else:
-        log_mel_spec = extract(fn)
+        spectra = extract(fn)
 
     if return_values:
-        if out_fn:
-            return out_fn, log_mel_spec
-        return log_mel_spec
-    elif out_fn:
-        return out_fn
+        if len(out_fns) > 0:
+            return out_fns, spectra
+        return spectra
+    return out_fns
--- feature_extraction/extract.py
+++ feature_extraction/extract.py
@@ -12,7 +12,7 @@ from sklearn.preprocessing import StandardScaler
 import joblib
 from tqdm import tqdm
 
-from feature_extraction.audio import log_mel_fbe
+from audio import log_mel_fbe
 
 
 nfft = 1024
@@ -20,17 +20,32 @@ n_mels = 64
 dim = (n_mels, 132, 1)
 n_classes = 41
 sampling_rate = 16000
-duration = 2
+duration = 3
 input_length = int(sampling_rate * duration)
 frame_size_ms = 25
-hop_size_ms = 15
+hop_size_ms = 10
 frame_length = int(sampling_rate * frame_size_ms / 1000.0)
 hop_length = int(sampling_rate * hop_size_ms / 1000.0)
 overlap_length = frame_length - hop_length
 window = np.hanning(frame_length)
 
 
+def mp_with_pbar(func, args, n_processes = 2):
+    p = mp.Pool(n_processes)
+    res_list = []
+    with tqdm(total = len(args)) as pbar:
+        for i, res in tqdm(enumerate(p.imap_unordered(func, args))):
+            pbar.update()
+            res_list.append(res)
+
+        pbar.close()
+        p.close()
+        p.join()
+    return res_list
+
+
 def extract(audio_dir, meta_fn, train_scaler=False):
+    df = pd.read_csv(meta_fn)
     fns = list(df['fname'].map(lambda fn: path.join(audio_dir, fn)))
     cur_batch_size = len(fns)
     df = pd.read_csv(meta_fn)
@@ -38,13 +53,18 @@ def extract(audio_dir, meta_fn, train_scaler=False):
     y = np.empty((cur_batch_size, n_classes))
     f_to_mel = filters.mel(sr=sampling_rate, n_fft=nfft, n_mels=n_mels)
 
-    with mp.Pool(mp.cpu_count()) as pool:
-        mp_func = partial(log_mel_fbe, return_values=False, output_dir="features", sampling_rate=sampling_rate,
-                         nfft=nfft, n_mels=n_mels, duration=duration,
-                         frame_length=frame_length, hop_length=hop_length, window=window)
-        feature_fns = pool.map_async(mp_func, fns).get()
+    print("Extracting features")
+    mp_func = partial(log_mel_fbe, return_values=False, output_dir="features", sampling_rate=sampling_rate,
+                     nfft=nfft, n_mels=n_mels, duration=duration,
+                     frame_length=frame_length, hop_length=hop_length, window=window, force=True)
+    feature_fn_groups = mp_with_pbar(mp_func, fns, mp.cpu_count())
+
+    feature_fns = list()
+    for group in feature_fn_groups:
+        feature_fns += group
 
     if train_scaler:
+        print("Training scaler")
         scaler = StandardScaler()
         for i, fn in tqdm(enumerate(feature_fns), total=len(feature_fns)):
             spec = np.load(fn)['spec']
@@ -55,12 +75,12 @@ def extract(audio_dir, meta_fn, train_scaler=False):
 
 if __name__ == '__main__':
     extract(
-        "/home/aagnone/corpora/freesound-audio-tagging/audio_train",
-        "/home/aagnone/corpora/freesound-audio-tagging/train_post_competition.csv",
+        "/corpora/freesound-audio-tagging/audio_train",
+        "/corpora/freesound-audio-tagging/train_post_competition.csv",
         train_scaler=True
     )
     extract(
-        "/home/aagnone/corpora/freesound-audio-tagging/audio_test",
-        "/home/aagnone/corpora/freesound-audio-tagging/test_post_competition_scoring_clips.csv",
+        "/corpora/freesound-audio-tagging/audio_test",
+        "/corpora/freesound-audio-tagging/test_post_competition_scoring_clips.csv",
         train_scaler=False
     )
